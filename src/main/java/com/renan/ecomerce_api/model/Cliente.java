d174ea3226c7b16bfdc3d309227db3bd40329a52
Thiago Renan <thiagorenan1108@gmail.com>
2026-04-09 19:57:49 -0300

    Criando entidade - Cliente

---
+++ src/main/java/com/renan/ecomerce_api/model/Cliente.java
@@ -0,0 +1,53 @@
+package com.renan.ecomerce_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    public Cliente(){}
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
